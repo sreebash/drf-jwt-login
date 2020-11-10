24ef2cecf807b9c009312b3e08fc0eb0f70ff47d
Sreebash <sreebashdas.1@gmail.com>
2020-11-11 04:35:07 +0600

    modified at settings.py file agian

--- grayspaceit/settings.py
+++ grayspaceit/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'authentication.UserAccount'
 
